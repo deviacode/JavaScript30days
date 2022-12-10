672b9fe5ad4c1908cd1f52a058447867a2c27373
Juan Devia <73263415+deviacode@users.noreply.github.com>
2022-12-10 17:46:42 -0500

    Eighteenth day of challenge
    Usando map() y reduce(), llevar el valor de las string de tiempo individual a un solo valor.

---
+++ 18_Adding_Up_Times/index.js
@@ -0,0 +1,22 @@
+const nodeTimes = Array.from(document.querySelectorAll("[data-time]"));
+
+const seconds = nodeTimes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
+
+document.write(
+  "El total de tiempo del vídeo es: " + hours + ":" + mins + ":" + secondsLeft
+);
